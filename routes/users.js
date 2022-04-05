1a75be8b5a81bec8c567983792438ac783a85ae0
DropboxKenshiro <jatujestem144@gmail.com>
2022-04-05 10:24:29 +0200

    Initial user registration implementation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
+var crypto = require("crypto");
+
 var express = require('express');
 var router = express.Router();
+var {User} = require('../models/models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', async function(req, res, next) {
+  try {
+    const hasher = crypto.createHash('sha256');
+    const saltUUID = crypto.randomUUID();
+    hasher.update(req.body.password + saltUUID);
+
+    await User.create({
+      email: req.body.email,
+      password: hasher.digest('hex'),
+      salt: saltUUID,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      dateOfBirth: req.body.dateOfBirth,
+      adressString: req.body.adressString,
+      zipCode: req.body.zipCode,
+      location: req.body.location,
+      description: req.body.description
+    });
+    res.status(200).json(
+      {
+        success: true
+      }
+    )
+  }
+  catch (err) {
+    res.status(400).json(
+      {
+        success: false,
+        errorType: err.name,
+        errorDescription: err.message
+      }
+    )
+  }
 });
+
+router.get('/info', function(req, res, next) {
+
+})
 
 module.exports = router;
