2386c86bb7179fa46c9421a1bac9d6994beda41b
DropboxKenshiro <jatujestem144@gmail.com>
2022-04-08 11:24:43 +0200

    Changed error rendering to sending JSON to avoid errors

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({error: res.locals.error, message: res.locals.message})
 });
 
 module.exports = app;
