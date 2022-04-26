aa9452cec9c0c0bf8a5a90a2507e0d6520c021be
DropboxKenshiro <jatujestem144@gmail.com>
2022-04-26 10:08:22 +0200

    Removed dummy route and added api specification fetching endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs/promises");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({response: 10})
+router.get('/apispec', async function(req, res, next) {
+  try {
+    res.json(JSON.parse(await fs.readFile('openapi/openapi.json',{encoding: 'utf-8'})))
+  }
+  catch (err) {
+    res.status(500).send("OpenAPI definion malformed. Please notify server owner.");
+  }
+  
 });
 
 module.exports = router;
