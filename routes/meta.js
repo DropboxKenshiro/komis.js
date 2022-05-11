9b99cb296493e3327756b5be523fc5bb4d1135d8
DropboxKenshiro <jatujestem144@gmail.com>
2022-05-11 14:22:15 +0200

    Initial meta routes

---
+++ routes/meta.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {makeErrorJson} = require('../utils/misc');
+const {Country} = require('../models/models');
+
+router.get("/countries", async function (req, res, next) {
+    try {
+        const rawResults = await Country.findAll({
+            attributes: ["name"]
+        });
+        res.status(200).json(rawResults.map(x => x["name"]));
+    }
+    catch (e) {
+        res.status(400).json(makeErrorJson(e));
+    }
+})
+
+module.exports = router;
