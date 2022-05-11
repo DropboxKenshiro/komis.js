264137bbae502671084e6859e8f5143abd5918d7
DropboxKenshiro <jatujestem144@gmail.com>
2022-05-11 14:25:03 +0200

    Added engines meta route

--- routes/meta.js
+++ routes/meta.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {makeErrorJson} = require('../utils/misc');
-const {Country} = require('../models/models');
+const {Country, EngineType} = require('../models/models');
 
 router.get("/countries", async function (req, res, next) {
     try {
@@ -13,6 +13,18 @@ router.get("/countries", async function (req, res, next) {
     catch (e) {
         res.status(400).json(makeErrorJson(e));
     }
-})
+});
+
+router.get("/engines", async function (req, res, next) {
+    try {
+        const rawResults = await EngineType.findAll({
+            attributes: ["name"]
+        });
+        res.status(200).json(rawResults.map(x => x["name"]));
+    }
+    catch (e) {
+        res.status(400).json(makeErrorJson(e));
+    }
+});
 
 module.exports = router;
